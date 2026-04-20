44ba4167b36a39496329fcfe7fb0fc60c94c2778
ImAguss <jellow022@gmail.com>
2026-04-20 15:07:52 -0300

    Correcciones de logica y agregado manejo de errores para emisor. Logica del nivel 1 casi completa

--- src/core/emisor.py
+++ src/core/emisor.py
@@ -2,12 +2,24 @@ import socket
 
 class Emisor:
 
-    def __init__(self, ruta, IP):
+    def __init__(self, ruta, IP, puerto):
         self.__ruta = ruta
         self.__IP = IP
+        self.__puerto = puerto
 
     def iniciar_conexion(self):
         socket_emisor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket_emisor.connect((self.__IP,5000))
-        socket_emisor.send(b"Pene")
-        socket_emisor.close()
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as emisor:
+            try:
+                emisor.connect((self.__IP,self.__puerto))
+                print(f"Emisor conectado a la IP: {self.__IP}, en el puerto: {self.__puerto}")
+
+                print("Enviando Datos...")
+                emisor.sendall(b"Hola mundo")
+
+            except Exception as error:
+                print(f"Ocurrio un error al realizar la conexion con {self.__IP} en el puerto {self.__puerto}, error: {error}")
+            finally:
+                emisor.close()
+                print("Conexion finalizada")
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import socket
+import time
 import tkinter as Tk
 import threading
 
@@ -7,27 +8,72 @@ from tkinter import filedialog
 from pathlib import Path
 
 def iniciar_servidor():
-    unSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    unSocket.bind(('0.0.0.0',5000))
-    unSocket.listen(5)
+    """
+    Esta funcion se encarga de hacer que la PC actue como servidor y que pueda recibir
+    archivos, ya que sera usado con un hilo para quedar en ejecucion esperando una 
+    conexion.
 
-    while True:
-        conexion, IP = unSocket.accept()
-        mensaje = conexion.recv(1024)
-        print(mensaje.decode('utf-8'))
-        print(f"Conexion establecida con {IP}")
+    """
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as servidor:
+            servidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            servidor.bind(("0.0.0.0",5000))
+            servidor.listen(5)
+            print("Conexion Exitosa!")
+
+            while True:
+                try:
+                    socket_conectado, IP = servidor.accept()
+                    while True:
+                        "Este bucle es para probar la logica del emisor"
+                        archivo = socket_conectado.recv(1024)
+
+                        if not archivo:
+                            print("Datos enviados correctamente")
+                            break
+
+                        print(archivo.decode("utf-8"))
+                except Exception as e:
+                    print(f"Conexion finalizada por un error: {e}")
+    except OSError as error:
+        pass
+            
+
 
 def seleccionar_archivos():
+    """
+    Me da paja hacer la logica del autocompletado para la terminal
+    de momento usaremos esto.
+    """
+
     root = Tk.Tk()
     root.withdraw()
     ruta = filedialog.askopenfilename(title="Selecciona el archivo a enviar")
     return ruta
 
 if __name__ == "__main__":
-    unHilo = threading.Thread(target=iniciar_servidor,daemon=True)
-    unHilo.start()
 
-        #ruta = seleccionar_archivos()
-        #IP = input("Direccion IP\n")
-    unEmisor = Emisor("nada","127.0.0.1")
-    unEmisor.iniciar_conexion()
+    try:
+        unHilo = threading.Thread(target=iniciar_servidor,daemon=True)
+        unHilo.start()
+        time.sleep(0.5)
+    except Exception as error:
+        print(f"Fallo al crear el hilo de ejecucion del servidor")
+
+
+
+    while True:
+        print("Seleccione las Opciones:")
+        op = int(input("""
+        1: Emisor
+        2: Receptor
+        """))
+
+        if op == 1:
+            unEmisor = Emisor("nada","127.0.0.1", 5000)
+            unEmisor.iniciar_conexion()
+        if op == 2:
+            print("Esperando Conexiones...")
+            break
+
+    unHilo.join()
