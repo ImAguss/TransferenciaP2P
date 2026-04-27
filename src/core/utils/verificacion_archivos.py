e6680f44950e9fbb2ab6a61d483eca1980c8c5e6
ImAguss <jellow022@gmail.com>
2026-04-27 19:17:30 -0300

    Programa funcional y bonito, correccion de bugs y errores solucionado.

--- src/core/utils/verificacion_archivos.py
+++ src/core/utils/verificacion_archivos.py
@@ -1,14 +1,13 @@
 import hashlib
 
 class VerificacionArchivos:
-    hashing = hashlib.sha256()
 
     def GenerarHashArchivo(self, ruta):
         """
-        Calcula el hash descomprimiendo el archivo en pedazos para poder
+        Calcula el hash del archivo en pedazos para poder
         ir actualizando el hash con update.
         """
-
+        hashing = hashlib.sha256()
         tamanio_chunk = 4096
         
         with open(ruta, "rb") as archivo:
@@ -16,7 +15,7 @@ class VerificacionArchivos:
                 chunk = archivo.read(tamanio_chunk)
                 if not chunk:
                     break
-                self.__hashing.update(chunk)
+                hashing.update(chunk)
 
         # Retorna el valor en Hexadecimal
         return hashing.hexdigest()
